50a4697c8db38aaeeb3cc86c7ffa488a7a42c6c2
kaiocosta22 <140025887+kaiocosta22@users.noreply.github.com>
2024-03-07 17:39:54 -0300

    Add files via upload

--- portifolio-dev/home.js
+++ portifolio-dev/home.js
@@ -1,2 +1,10 @@
-const btn = document.querySelector('.btn');
-const 
+let icon = document.getElementById('icon')
+
+icon.onclick = function(){
+    document.body.classList.toggle('dark-theme')
+    if(document.body.classList.contains('dark-theme')){
+        icon.src = 'img/sun.png'
+    }else{
+        icon.src = 'img/moon.png'
+    }
+}
